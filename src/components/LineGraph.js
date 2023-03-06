6de1ac9cca5b4dcd8b76cb1549d46f07955d017e
Cole Anthony <cole.edwin.anthony@gmail.com>
2023-03-05 22:23:16 -0700

    add line graph

---
+++ src/components/LineGraph.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis,
+  VictoryLabel,
+  VictoryTheme,
+} from "victory";
+
+const LineGraph = ({data}) => {
+
+  const tempData =
+    data.length !== 0
+      ? data.days[0].hours.slice(4, 21).reduce((acc, hour) => {
+          acc.push({
+            x: Number(hour.datetime.slice(0, 2)),
+            y: Math.round(hour.temp),
+          });
+          return acc;
+        }, [])
+      : null;
+console.log(tempData)
+  return (
+    <div>
+      <VictoryChart theme={VictoryTheme.material} domainPadding={{ x: 1, y: 20 }}>
+      <VictoryAxis dependentAxis tickFormat={(y) => `${y}\u00B0F`} />
+        <VictoryAxis
+          tickValues={[4, 8, 12, 16, 20]}
+          tickFormat={["4 A.M", "8 A.M", "Noon", "4 P.M", "8 P.M."]}
+        />
+      <VictoryLine
+      data={tempData}
+      />
+      </VictoryChart>
+    </div>
+  )
+}
+
+export default LineGraph
