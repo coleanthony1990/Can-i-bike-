0aaa2dac813806b59889b8e79610a1361f71a565
Cole Anthony <cole.edwin.anthony@gmail.com>
2023-03-10 14:14:06 -0700

    remove timezones

--- src/components/Current.js
+++ src/components/Current.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import Condition from './Condition'
 import {FiWind, FiClock} from 'react-icons/fi'
 import {BsThermometerHalf, BsDropletHalf} from 'react-icons/bs'
-import {WiHumidity} from 'react-icons/wi'
 import {ImCloud} from 'react-icons/im'
 import {IoRainySharp} from 'react-icons/io5'
 
@@ -10,21 +8,8 @@ import {IoRainySharp} from 'react-icons/io5'
 
 const Current = ({data}) => {
   
-  const bigData = data.length !== 0 ? data : null
-  const currentKeys = data.length !== 0 ? Object.keys(data.currentConditions) : null
-  
-  const conditions = data.length !== 0 ? currentKeys.map((thing, index) => {
-    
-    return (
-      <Condition
-      info={data.currentConditions[thing]}
-      stuff={thing}
-      key={index}
-      
-      />
-    )
-  })
-   : null
+  // const bigData = data.length !== 0 ? data : null
+
   const temp = data.length !== 0 ? ` ${data.currentConditions.temp}\u00B0F` : null
   const feelslike = data.length !== 0 ? ` ${data.currentConditions.feelslike}\u00B0F` : null
   const humidity = data.length !== 0 ? ` ${data.currentConditions.humidity}%` : null
@@ -34,7 +19,7 @@ const Current = ({data}) => {
 
   return (
     <div>
-      {data.length !== 0 && <p>Right now in {bigData.timezone.split('/')[1]}</p>}
+      {/* {data.length !== 0 && <p>Right now in {bigData.timezone.split('/')[1]}</p>} */}
       {data.length !== 0 && <p className='time'><FiClock/> {data.currentConditions.datetime}</p>}
       <div className='conditions-data'>
       <p className='temp'><BsThermometerHalf/>Current temperature: {temp}</p>
